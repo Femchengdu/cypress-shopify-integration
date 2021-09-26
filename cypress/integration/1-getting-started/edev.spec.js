86bb7225d9880cb7493cd97b710833686aee70f6
Samuel Ajayi <femiajayi705@gmail.com>
2021-09-26 21:12:49 +0200

    refac: Testing commit 8 added semi colon

--- cypress/integration/1-getting-started/edev.spec.js
+++ cypress/integration/1-getting-started/edev.spec.js
@@ -1,7 +1,7 @@
 describe('Home page confirmation', () => {
     it('successfully loads the home page!!!', () => {
       cy.visit('https://cypress-integration.myshopify.com'); // change URL to match your dev URL
-    })
+    });
     it("Finds the `Talk about your brand` header on the page!!", ()=>{
        cy.visit('https://cypress-integration.myshopify.com'); // change URL to match your dev URL
         cy.contains('span', 'Talk about your brand');
