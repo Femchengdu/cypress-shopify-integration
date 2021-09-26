f701efd2aad1c231927c293843a4f8ef85ff4bbd
Samuel Ajayi <femiajayi705@gmail.com>
2021-09-26 21:16:21 +0200

    refac: Testing commit 9 run 2 in same it block

--- cypress/integration/1-getting-started/edev.spec.js
+++ cypress/integration/1-getting-started/edev.spec.js
@@ -1,9 +1,9 @@
 describe('Home page confirmation', () => {
     it('successfully loads the home page!!!', () => {
       cy.visit('https://cypress-integration.myshopify.com'); // change URL to match your dev URL
+      cy.contains('span', 'Talk about your brand');
     });
-    it("Finds the `Talk about your brand` header on the page!!", ()=>{
-       cy.visit('https://cypress-integration.myshopify.com'); // change URL to match your dev URL
-        cy.contains('span', 'Talk about your brand');
-    })
+    // it("Finds the `Talk about your brand` header on the page!!", ()=>{
+    //    cy.visit('https://cypress-integration.myshopify.com'); // change URL to match your dev URL
+    // })
 })
